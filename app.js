4f31872e430d506dbc0204235eb3b90b98d2b029
Leo Chen <minghaoc@andrew.cmu.edu>
2023-09-18 18:44:49 -0700

    Integrate Swagger Document

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+// const bodyParser = require('body-parser');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const indexRouter = require('./src/routes/indexRouter');
 const userRouter = require('./src/routes/userRouter');
@@ -15,8 +18,41 @@ app.use(express.static('src/public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
 
+const options = {
+  definition: {
+    openapi: '3.1.0',
+    info: {
+      title: 'ESN Backend API',
+      version: '0.0.1',
+      description:
+        'This is a simple CRUD API application made with Express and documented with Swagger',
+      license: {
+        name: 'MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      contact: {
+        name: 'LogRocket',
+        url: 'https://logrocket.com',
+        email: 'info@email.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
 app.use(indexRouter);
 app.use(userRouter);
+const specs = swaggerJsdoc(options);
+app.use(
+  '/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true }),
+);
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,3 +1,24 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - username
+ *         - password
+ *       properties:
+ *         username:
+ *           type: string
+ *           description: User's username
+ *         password:
+ *           type: string
+ *           description: User's password
+ *       example:
+ *         username: testUser
+ *         password: userpassword
+ */
+
 const express = require('express');
 const UserController = require('../controllers/userController');
 
@@ -7,7 +28,46 @@ router.get('/register', (req, res) => {
   res.render('register');
 });
 
+/**
+  * @swagger
+  * /api/users/validateUser:
+  *   post:
+  *     description: validate user
+  *     tags: [Users]
+  *     requestBody:
+  *       required: true
+  *       content:
+  *         application/json:
+  *           schema:
+  *             $ref: '#/components/schemas/User'
+  *     responses:
+  *       200:
+  *         description: validation success
+  *         schema:
+  *           type: object
+  *           $ref: '#/components/schemas/User'
+*/
 router.post('/api/users/validateUser', UserController.validateUser);
+
+/**
+  * @swagger
+  * /api/users/register:
+  *   post:
+  *     description:
+  *     tags: [Users]
+  *     requestBody:
+  *       required: true
+  *       content:
+  *         application/json:
+  *           schema:
+  *             $ref: '#/components/schemas/User'
+  *     responses:
+  *       200:
+  *         description: registration success
+  *         schema:
+  *           type: object
+  *           $ref: '#/components/schemas/User'
+*/
 router.post('/api/users/register', UserController.registerUser);
 
 module.exports = router;
