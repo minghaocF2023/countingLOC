1f384c1786a42dd4be8b72b9b458c1ba83ec65f9
Yunxiao Xu <yunxiaox@andrew.cmu.edu>
2023-09-27 16:38:42 -0700

    Complete user api docs

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,27 +2,57 @@
  * @swagger
  * components:
  *   schemas:
- *     User:
+ *     Username:
+ *       type: string
+ *       description: User's username
+ *       example: testUser
+ *     UserPassword:
+ *        type: string
+ *        description: User's password
+ *        example: testPassword
+ *     UsernameList:
+ *       type: array
+ *       description: List of usernames
+ *       items:
+ *         $ref: '#/components/schemas/Username'
+ *     NewUser:
  *       type: object
  *       required:
  *         - username
  *         - password
  *       properties:
  *         username:
- *           type: string
- *           description: User's username
- *           example: testUser
+ *           $ref: '#/components/schemas/Username'
  *         password:
- *           type: string
- *           description: User's password
- *           example: testPassword
+ *           $ref: '#/components/schemas/UserPassword'
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
  *     Response:
  *       type: object
  *       properties:
  *         message:
  *           type: string
  *           description: Response message
+ *           example: OK
  */
+
+/**
+ * @swagger
+ * parameters:
+ *   username:
+ *     name: username
+ *     in: path
+ *     description: Username of a user
+ *     required: true
+ *     schema:
+ *       type: string
+ *     example: testUser
+ */
+
 const express = require('express');
 const UserController = require('../controllers/userController');
 
@@ -35,6 +65,26 @@ const router = express.Router();
  *     tags: [Users]
  *     summary: Get all users
  *     description: Get a list of all registered usernames and a list of banned usernames
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/Response'
+ *                 - type: object
+ *                   properties:
+ *                     users:
+ *                       $ref: '#/components/schemas/UsernameList'
+ *                       description: List of registered usernames
+ *                     banned_users:
+ *                       $ref: '#/components/schemas/UsernameList'
+ *                       description: List of banned usernames
+ *             example:
+ *               message: OK
+ *               users: [testUser1, testUser2]
+ *               banned_users: [bannedUser1, bannedUser2]
  */
 router.get('/', UserController.getAllUsers);
 
@@ -45,6 +95,33 @@ router.get('/', UserController.getAllUsers);
  *     tags: [Users]
  *     summary: Get user by username
  *     description: Get a user by username
+ *     parameters:
+ *       - $ref: '#/parameters/username'
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/Response'
+ *                 - type: object
+ *                   properties:
+ *                     user:
+ *                       $ref: '#/components/schemas/Username'
+ *       404:
+ *         description: Not a registered user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Response'
+ *             examples:
+ *               userNotFound:
+ *                 value:
+ *                   message: User not found
+ *               bannedUser:
+ *                 value:
+ *                   message: Banned username
  */
 router.get('/:username', UserController.getUserByUsername);
 
@@ -55,6 +132,40 @@ router.get('/:username', UserController.getUserByUsername);
  *     tags: [Users]
  *     summary: Create a new user
  *     description: Create a new user with username and password
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/NewUser'
+ *     responses:
+ *       201:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *              allOf:
+ *                - $ref: '#/components/schemas/Response'
+ *                - type: object
+ *                  properties:
+ *                    user:
+ *                      $ref: '#/components/schemas/Username'
+ *       400:
+ *         description: Invalid request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Response'
+ *             example:
+ *               message: Invalid request
+ *       405:
+ *         description: Duplicated username
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Response'
+ *             example:
+ *               message: Duplicated username
  */
 router.post('/', UserController.createUser);
 
@@ -65,6 +176,47 @@ router.post('/', UserController.createUser);
  *     tags: [Users]
  *     summary: login
  *     description: login a user with password
+ *     parameters:
+ *       - $ref: '#/parameters/username'
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - password
+ *             properties:
+ *               password:
+ *                 $ref: '#/components/schemas/UserPassword'
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *              allOf:
+ *                - $ref: '#/components/schemas/Response'
+ *                - type: object
+ *                  properties:
+ *                    user:
+ *                      $ref: '#/components/schemas/Username'
+ *       400:
+ *         description: Invalid request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Response'
+ *             example:
+ *               message: Invalid request
+ *       404:
+ *         description: Fail to login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Response'
+ *             example:
+ *               message: Incorrect username/password
  */
 router.put('/:username/online', UserController.loginUser);
 
@@ -75,6 +227,44 @@ router.put('/:username/online', UserController.loginUser);
  *     tags: [Users]
  *     summary: logout
  *     description: logout a user
+ *     parameters:
+ *       - $ref: '#/parameters/username'
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *              allOf:
+ *                - $ref: '#/components/schemas/Response'
+ *                - type: object
+ *                  properties:
+ *                    user:
+ *                      $ref: '#/components/schemas/Username'
+ *       400:
+ *         description: Invalid request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Response'
+ *             example:
+ *               message: Invalid request
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Response'
+ *             example:
+ *               message: User not found
+ *       405:
+ *         description: Fail to logout
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Response'
+ *             example:
+ *               message: User not logged in
  */
 router.put('/:username/offline', UserController.logoutUser);
 
@@ -90,13 +280,13 @@ router.put('/:username/offline', UserController.logoutUser);
   *       content:
   *         application/json:
   *           schema:
-  *             $ref: '#/components/schemas/User'
+  *             $ref: '#/components/schemas/NewUser'
   *     responses:
   *       200:
   *         description: validation success
   *         schema:
   *           type: object
-  *           $ref: '#/components/schemas/User'
+  *           $ref: '#/components/schemas/NewUser'
 */
 router.post('/validate', UserController.validate);
 
@@ -112,13 +302,13 @@ router.post('/validate', UserController.validate);
   *       content:
   *         application/json:
   *           schema:
-  *             $ref: '#/components/schemas/User'
+  *             $ref: '#/components/schemas/NewUser'
   *     responses:
   *       200:
   *         description: registration success
   *         schema:
   *           type: object
-  *           $ref: '#/components/schemas/User'
+  *           $ref: '#/components/schemas/NewUser'
 */
 router.post('/register', UserController.createUser);
 
