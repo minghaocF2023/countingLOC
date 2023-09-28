46fc33afb3177496787e8a1c9e890d5262c10ced
Yunxiao Xu <yunxiaox@andrew.cmu.edu>
2023-09-27 18:42:58 -0700

    Complete message api docs

--- src/routes/messageRouter.js
+++ src/routes/messageRouter.js
@@ -1,3 +1,40 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     NewMessage:
+ *       type: object
+ *       description: New message
+ *       required:
+ *         - author
+ *         - content
+ *       properties:
+ *         author:
+ *           $ref: '#/components/schemas/Username'
+ *           description: Message author
+ *         content:
+ *           type: string
+ *           description: Message content
+ *           example: Hello World!
+ *     Message:
+ *       allOf:
+ *         - $ref: '#/components/schemas/NewMessage'
+ *         - type: object
+ *           properties:
+ *             mid:
+ *               type: string
+ *               description: Message ID
+ *               example: 650b635c9d39c281eac33bbf
+ *             status:
+ *               type: string
+ *               description: Message status
+ *               example: OK
+ *             createdAt:
+ *               type: string
+ *               description: Message creation time
+ *               example: 1970-01-01 00:00:00
+ */
+
 const express = require('express');
 const MessageController = require('../controllers/messageController');
 
@@ -10,18 +47,32 @@ const router = express.Router();
  *     tags: [Messages]
  *     summary: Get all public messages
  *     description: Get all messages published on the public wall
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/Response'
+ *                 - type: object
+ *                   properties:
+ *                     messages:
+ *                       type: array
+ *                       items:
+ *                        $ref: '#/components/schemas/Message'
  */
 router.get('/public', MessageController.getAllPublicMessages);
 
-/**
- * @swagger
- * /api/messages/public/{username}:
- *   get:
- *     tags: [Messages]
- *     summary: Get all public messages sent by a user
- *     description: Get all messages published on the public wall by a user
- */
-router.get('/public/:username', MessageController.getPublicMessageByUsername);
+// /**
+//  * @swagger
+//  * /api/messages/public/{username}:
+//  *   get:
+//  *     tags: [Messages]
+//  *     summary: Get all public messages sent by a user
+//  *     description: Get all messages published on the public wall by a user
+//  */
+// router.get('/public/:username', MessageController.getPublicMessageByUsername);
 
 /**
  * @swagger
@@ -30,6 +81,42 @@ router.get('/public/:username', MessageController.getPublicMessageByUsername);
  *     tags: [Messages]
  *     summary: Post a new public message
  *     description: Post a new message on the public wall
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/NewMessage'
+ *     responses:
+ *       201:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/Response'
+ *                 - type: object
+ *                   properties:
+ *                     mid:
+ *                       type: string
+ *                       description: Message ID
+ *                       example: 650b635c9d39c281eac33bbf
+ *       400:
+ *         description: Invalid request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Response'
+ *             example:
+ *               message: Invalid request
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Response'
+ *             example:
+ *               message: User not found
  */
 router.post('/public', MessageController.createPublicMessage);
 
