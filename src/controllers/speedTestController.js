d1be3f2b3760ebe197eb70179ff21f993766c7f2
Leo Chen <minghaoc@andrew.cmu.edu>
2023-10-16 21:12:10 -0700

    modify start speed test unfinished

--- src/controllers/speedTestController.js
+++ src/controllers/speedTestController.js
@@ -3,39 +3,53 @@
 import { testConnection } from '../services/db.js';
 import publicMessageFactory from '../models/publicMessageModel.js';
 
-let testState = false;
-const getTestState = () => testState;
-const setIsTest = (isTest) => {
-  testState = isTest;
-};
-const getIsTestState = (req, res) => {
-  res.status(200).json({ message: 'OK', isTest: getTestState });
-};
-const startSpeedTest = async (req, res) => {
-  const { isTest } = req.body;
-  setIsTest(isTest);
-  console.log('Speed test started');
-  const PublicTestMessage = publicMessageFactory(testConnection);
+class speedTestController {
+  testState = false;
 
-  // Create a new test message
-  const testMessage = new PublicTestMessage({
-    content: 'Test content',
-    senderName: 'Test sender',
-    timestamp: Date.now(),
-    status: 'Test status',
-  });
+  static getState = () => this.testState;
 
-  // Save the test message to the database
-  try {
-    await testMessage.save();
-    console.log('Test message saved to the database');
-    res.status(200).json({ message: 'OK' });
-  } catch (error) {
-    console.error('Failed to save test message:', error);
-    res.status(500).json({ message: 'Failed to save test message' });
+  static setState = (state) => {
+    this.testState = state;
+  };
+
+  /**
+   * to check if current system is in performance testing state
+   */
+  static async getIsTestState(req, res) {
+    res.status(200).json({ message: 'OK', isTest: this.testState });
   }
-};
 
-export {
-  getTestState, setIsTest, startSpeedTest, getIsTestState,
-};
+  static async startSpeedTest(req, res) {
+    const { isTest, duration, interval } = req.body;
+    this.setState(isTest);
+    // console.log('Speed test started');
+    const PublicTestMessage = publicMessageFactory(testConnection);
+
+    // socket notify users that start testing!
+    const socketServer = req.app.get('socketServer');
+    socketServer.publishEvent('speedtest', { test: true });
+
+    // Create a new test message
+    const testMessage = new PublicTestMessage({
+      content: 'This is a 20 word ms',
+      senderName: 'Test sender',
+      timestamp: Date.now(),
+      status: 'GREEN',
+    });
+
+    // Save the test message to the database
+    try {
+      await testMessage.save();
+      console.log('Test message saved to the database');
+      res.status(200).json({ message: 'OK' });
+    } catch (error) {
+      console.error('Failed to save test message:', error);
+      res.status(500).json({ message: 'Failed to save test message' });
+    }
+  }
+
+  static async getMessageBetweenUsers(req, res) {
+    res.status(501).json({ message: 'Not implemented' });
+  }
+}
+export default speedTestController;
